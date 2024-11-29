8f3a148babb1a84a0fe7a36c02cead7ac552c713
Drazhin Ustin <zmagar91@gmail.com>
2024-11-29 20:18:09 +0300

    Setup project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+module.exports = config;
